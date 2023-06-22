2085a58dcb6fe320d264d8bb6f01143a34b8c0ad
KhoiUna <thorwaitson@gmail.com>
2023-06-22 11:51:40 -0500

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { Web5 } from '@tbd54566975/web5';
 import { Record } from '@tbd54566975/web5/dist/types/record';
-import { SyntheticEvent, useEffect, useRef, useState } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
+// @ts-ignore
+import * as TBD from '../node_modules/@tbd54566975/web5/dist/browser';
+const { Web5 } = TBD
 
 type Todo = {
   record: Record
@@ -13,52 +15,58 @@ type Todo = {
 }
 
 export default function Home() {
-  const web5 = useRef<Web5>()
-  const myDid = useRef('')
-
+  const [web5, setWeb5] = useState<typeof Web5>()
+  const [myDid, setMyDid] = useState('')
   const [todos, setTodos] = useState<Todo[]>([])
 
   useEffect(() => {
     const initWeb5 = async () => {
-      const { web5: currentWeb5, did } = await Web5.connect()
-      web5.current = currentWeb5
-      myDid.current = did
+      try {
+        const { web5, did } = await Web5.connect()
+        setWeb5(web5)
+        setMyDid(did)
+      } catch (error) {
+        console.error(error);
+      }
     }
     initWeb5()
   }, [])
 
   useEffect(() => {
     const getTodos = async () => {
-      if (!web5.current) return
+      try {
+        const { records } = await web5.dwn.records.query({
+          message: {
+            filter: {
+              schema: 'http://some-schema-registry.org/todo'
+            },
+          }
+        });
 
-      const { records } = await web5.current.dwn.records.query({
-        message: {
-          filter: {
-            schema: 'http://some-schema-registry.org/todo'
-          },
+        if (!records) return setTodos([])
+
+        const todos = []
+        for (let record of records) {
+          const data = await record.data.json()
+          todos.push({ record, data, id: record.id })
         }
-      });
 
-      if (!records) return setTodos([])
-
-      const todos = []
-      for (let record of records) {
-        const data = await record.data.json()
-        todos.push({ record, data, id: record.id })
+        return setTodos(todos)
+      } catch (error) {
+        console.error(error);
+        return setTodos([])
       }
-
-      return setTodos(todos)
     }
     getTodos()
-  }, [])
+  }, [web5])
 
   const [description, setDescription] = useState('')
   const addTodo = async (event: SyntheticEvent) => {
     event.preventDefault()
     try {
-      if (!web5.current) throw 'Error init Web5'
+      if (!web5) throw 'Error init Web5'
 
-      const { record } = await web5.current.dwn.records.create({
+      const { record } = await web5.dwn.records.create({
         data: { text: description },
         message: {
           schema: 'http://some-schema-registry.org/todo',
@@ -77,19 +85,20 @@ export default function Home() {
     }
   }
 
+
+
   return (
     <main className="bg-slate-100 p-10 min-h-screen m-auto">
       <div className="w-fit m-auto">
-        {todos.length === 0 && <p className='text-slate-500 text-center p-3 italic'>Nothing here yet!</p>}
         {todos.map((item, index) => (
           <TodoCard key={index} description={item.data.text} />
         ))}
 
-
         <form
           onSubmit={addTodo}
           className="w-[350px] h-[70px] bg-white drop-shadow-lg flex justify-around items-center p-3 my-3 rounded-lg text-slate-500">
-          <input autoComplete='off' className='text-black outline-none border-2 border-black p-2 rounded-lg' type="text"
+          <input autoComplete='off' className='text-black outline-none border-2 border-slate-300 p-2 rounded-lg' type="text"
+            placeholder='Type here'
             value={description}
             onChange={({ target }) => setDescription(target.value)}
           />
